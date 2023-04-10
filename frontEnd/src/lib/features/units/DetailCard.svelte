9eb456041d9835d8542533f818c786dab830e639
Eidamiino <adam.pala199@gmail.com>
2023-04-10 09:04:02 +0200

    delete button on item detail

--- frontEnd/src/lib/features/units/DetailCard.svelte
+++ frontEnd/src/lib/features/units/DetailCard.svelte
@@ -1,12 +1,27 @@
 <script>
     export let items;
     console.log(items);
+    import ModalComponent from "../ModalComponent.svelte";
+    import { deleteUnitAsync } from "./unit-provider";
+
+    let selectedItem = null;
+    const remove = async function () {
+        if (selectedItem) {
+            await deleteUnitAsync(selectedItem);
+            selectedItem = null;
+        }
+    };
+    let modalcomponent;
+    const showModal = (item) => {
+        selectedItem = item;
+        modalcomponent.show();
+    };
 </script>
 
 <form on:submit|preventDefault|stopPropagation>
     <div class="form-group row">
         <label for="itemName" class="col-sm-2 col-form-label">Name: </label>
-        <div class="col-sm-10">
+        <div class="col-sm-8">
             <input
                 type="text"
                 readonly
@@ -15,6 +30,29 @@
                 value={items[0].name}
                 style="color:black;"
             />
+        </div>
+        <div class="col-sm-2">
+            <button
+                on:click={() => showModal(items[0].name)}
+                class="btn btn-danger rounded-0"
+                type="button"
+                data-toggle="tooltip"
+                data-placement="top"
+                title="Delete"
+                style="text-align:right;"
+            >
+                <i class="fa fa-trash" style="padding: 0.5rem, 0.7rem;" />
+            </button>
+            <ModalComponent bind:this={modalcomponent}>
+                <h1 style="text-align:center;">Are you sure?</h1>
+                <button
+                    style="position:absolute;bottom: 1em;left:40%"
+                    on:click={async () => {
+                        console.log("removing:" + selectedItem);
+                        await remove();
+                    }}>Delete</button
+                >
+            </ModalComponent>
         </div>
     </div>
 </form>
